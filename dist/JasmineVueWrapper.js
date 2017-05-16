177f08d7d2b6178340d93a5b6b090e5f59916058
matt-oconnell <mattoconnell408@gmail.com>
2017-05-16 17:43:39 -0400

    0.0.7

--- dist/JasmineVueWrapper.js
+++ dist/JasmineVueWrapper.js
@@ -12,9 +12,15 @@ var _vue = require('vue');
 
 var _vue2 = _interopRequireDefault(_vue);
 
+var _vuex = require('vuex');
+
+var _vuex2 = _interopRequireDefault(_vuex);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+_vue2.default.use(_vuex2.default);
 
 var JasmineVueWrapper = function () {
   function JasmineVueWrapper(Component) {
@@ -32,13 +38,20 @@ var JasmineVueWrapper = function () {
     key: 'mount',
     value: function mount() {
       var propsData = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : this.defaultProps;
+      var store = arguments[1];
 
       var wrap = document.createElement('div');
       this.container.appendChild(wrap);
 
-      var vm = new _vue2.default(_extends({}, this.component, {
+      var componentData = _extends({}, this.component, {
         propsData: propsData
-      })).$mount(wrap);
+      });
+
+      if (store) {
+        componentData.store = new _vuex2.default.Store(store);
+      }
+
+      var vm = new _vue2.default(componentData).$mount(wrap);
 
       this.instanceCache.push(vm);
 
